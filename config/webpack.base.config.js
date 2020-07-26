c1450a58ff54675efb8a05c4147b9714b9be1ff0
caitoulin <1218182878@qq.com>
2020-07-26 17:07:27 +0800

    feat:配置完成

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -5,30 +5,34 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 let htmlWebpackPlugin = new HtmlWebpackPlugin({
   filename: "index.html",
-  template: path.resolve(__dirname, "./views/index.html"),
+  template: path.resolve(__dirname, "../views/index.html"),
 });
 
 let miniCssExtractPlugin = new MiniCssExtractPlugin({
-  filename: path.resolve(__dirname, "/views/dist/style.css"),
+  filename: path.resolve(__dirname, "../dist/style.css"),
 });
 module.exports = {
   devtool: "eval-source-map",
-  entry: __dirname + "/views/src/index.js",
-  output: { path: path.join(__dirname + "/views/dist"), filename: bundle.js },
-  modules: {
+  entry:['webpack-hot-middleware/client', path.resolve(__dirname, "../views/src/index.js")],
+  output: {
+    path: path.join(__dirname, "..", "/dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
     rules: [
       {
         test: /\.(less|css)$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
       },
       {
-        test: /\.js[x]$/,
+        test: /\.(jsx|js)$/,
         exclude: /node-modules/,
         loader: "babel-loader",
       },
       {
         test: /\.(img|jpg|gif|svg)$/,
-        lodader: "file-loader",
+        loader: "file-loader",
         options: {
           name: "images/[name].[ext]",
         },
@@ -39,5 +43,11 @@ module.exports = {
       },
     ],
   },
-  plugins: [htmlWebpackPlugin, miniCssExtractPlugin],
+  plugins: [
+    htmlWebpackPlugin,
+    miniCssExtractPlugin,
+    new webpack.DllReferencePlugin({
+      manifest: require("../dist/vendor-manifest.json"),
+    }),
+  ],
 };
